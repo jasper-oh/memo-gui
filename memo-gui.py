beb2af116e2af549009981c65e08197196dc55b0
jasper <63331153+jasper-oh@users.noreply.github.com>
2020-12-06 16:50:07 +0900

    edit menu bar & Error handle
    file, edit, view, window, help are added

--- memo-gui.py
+++ memo-gui.py
@@ -14,20 +14,26 @@ def save():
 
 
 menu = Menu(root)
+
 menu_file = Menu(menu, tearoff=0)
+menu_format = Menu(menu, tearoff=0)
+menu_edit = Menu(menu, tearoff=0)
+menu_view = Menu(menu, tearoff=0)
+menu_window = Menu(menu, tearoff=0)
+menu_help = Menu(menu, tearoff=0)
 
 menu_file.add_command(label="Open", command=open)
 menu_file.add_command(label="Save", command=save)
 menu_file.add_separator()
 menu_file.add_command(label="Close", command=root.quit)
 
-menu_file.add_cascade(label="File", menu=menu_file)
+menu.add_cascade(label="File", menu=menu_file)
 
-menu.add_cascade(label="Format")
-menu.add_cascade(label="Edit")
-menu.add_cascade(label="View")
-menu.add_cascade(label="Window")
-menu.add_cascade(label="Help")
+menu.add_cascade(label="Format", menu=menu_format)
+menu.add_cascade(label="Edit", menu=menu_edit)
+menu.add_cascade(label="View", menu=menu_view)
+menu.add_cascade(label="Window", menu=menu_window)
+menu.add_cascade(label="Help", menu=menu_help)
 
 
 root.config(menu=menu)
