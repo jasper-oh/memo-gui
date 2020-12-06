a972e3cd5c6f8b1252c93d7373bd4e6f58c56035
jasper <63331153+jasper-oh@users.noreply.github.com>
2020-12-06 16:59:08 +0900

    Writing area & Scroll-bar
    Make area & Scroll-bar

--- memo-gui.py
+++ memo-gui.py
@@ -5,4 +5,14 @@ root.title("Notes")
 root.geometry("640x480")
 
 
+# scroll bar
+scrollbar = Scrollbar(root)
+scrollbar.pack(side="right", fill="y")
+
+# Writing Area
+txt = Text(root, yscrollcommand=scrollbar.set)
+txt.pack(side="left", fill="both", expand=True)
+
+scrollbar.config(command=txt.yview)
+
 root.mainloop()
